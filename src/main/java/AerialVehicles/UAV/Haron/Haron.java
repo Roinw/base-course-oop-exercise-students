b77fe85983615f51cac5a68f7ae01a49e8ecab1e
Roi Weiss <roi.n.weiss2@gmail.com>
2020-06-08 16:11:00 +0300

    Change Haron to extends UAV

--- src/main/java/AerialVehicles/UAV/Haron/Haron.java
+++ src/main/java/AerialVehicles/UAV/Haron/Haron.java
@@ -1,8 +1,7 @@
 package AerialVehicles.UAV.Haron;
 
 import AerialVehicles.AerialAttackVehicle;
-import AerialVehicles.AerialIntelligenceVehicle;
-import AerialVehicles.AerialVehicle;
+import AerialVehicles.UAV.UAV;
 import Missions.Mission;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,17 +10,15 @@ import static Shard.ShardConstants.HARON_MAX_FLIGHT_HOURS_SINCE_LAST_REPAIR;
 
 @Getter
 @Setter
-public class Haron extends AerialVehicle implements AerialAttackVehicle, AerialIntelligenceVehicle {
+public class Haron extends UAV implements AerialAttackVehicle {
 
     protected String missileType;
     protected int numOfMissiles;
-    protected String sensorType;
 
     public Haron(int numOfMissiles, String missileType, String sensorType, String pilotName, Mission mission, int hoursOfFlightSinceLastRepair, boolean isReadyToFly) {
-        super(pilotName, mission, hoursOfFlightSinceLastRepair, isReadyToFly);
+        super(sensorType, pilotName, mission, hoursOfFlightSinceLastRepair, isReadyToFly);
         this.missileType = missileType;
         this.numOfMissiles = numOfMissiles;
-        this.sensorType = sensorType;
     }
 
     @Override
